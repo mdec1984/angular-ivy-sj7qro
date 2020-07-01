d08014eb0762eb144de686706f3c023df4552061
mdec1984 <mdec1984@gmail.com>
2020-07-01 20:12:00 +0200

    1 binaries uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component, VERSION } from '@angular/core';
+import {Component} from '@angular/core';
+import {User} from './classes/user';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  title = 'UMS';
+  showForm = false;
+  userSelected = new User();
+
+  updateUser(user: User) {
+    this.showForm = true;
+    this.userSelected = user;
+  }
+  newUser() {
+    this.userSelected = new User();
+    this.showForm = true;
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,27 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { UsersComponent } from './users/users.component';
+import { UserService } from './services/user.service';
+import { UserComponent } from './user/user.component';
+import {FormsModule} from '@angular/forms';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    UserDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
